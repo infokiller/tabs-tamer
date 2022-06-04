7190b2e0a347669b8ffa3a925ebfc8f997aaf804
Yoni Weill <joweill@icloud.com>
2022-06-04 12:55:10 +0300

    add plubming

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+/* global module */
+
+module.exports = {
+  env: {
+    es2017: true,
+    browser: true,
+    webextensions: true,
+  },
+  extends: ['eslint:recommended', 'google', 'plugin:json/recommended'],
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+    'require-jsdoc': 0,
+  },
+};
